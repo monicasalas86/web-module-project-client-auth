d47bd4d4e991f0f3efead6984bfbcb44a8147557
monicasalas86 <monicsalas167@gmail.com>
2021-10-12 23:42:42 -0500

    created PrivateRoute component

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
 
 import './App.css';
 
 function App() {
+  let isLoggedIn = localStorage.getItem('token');
+
   return (
     <Router>
       <div className="App">

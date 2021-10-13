44475b7663b401a9128190b562c316880f6d261c
monicasalas86 <monicsalas167@gmail.com>
2021-10-13 01:04:03 -0500

    created logout component

---
+++ friends/src/components/Logout.js
@@ -0,0 +1,24 @@
+import React, {useEffect} from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
+
+const Logout = (props) => {
+    useEffect(() => {
+        axiosWithAuth()
+        .post('logout')
+        .then(res => {
+            localStorage.removeItem('token')
+            props.history.push('/login')
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }, []);
+
+    return(
+        <div>
+            <p>see ya!</p>
+        </div>
+    )
+}
+
+export default Logout;
